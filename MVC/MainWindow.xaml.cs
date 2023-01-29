bec8f83a1db6876fd131ac6f21278a73788f6591
user228 <forfarmcard@mail.ru>
2023-01-29 11:40:34 +0300

    Добавьте файлы проекта.

---
+++ MVC/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVC
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Model.tbTextOperation = tbTextOperation;
+            Model.tbResult = tbResult;
+            cbOperation.ItemsSource = Model.dataListOperationStr;
+            cbOperation.SelectedIndex = 0;
+        }
+
+        private void cbOperation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Model.Operation = cbOperation.SelectedIndex;
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            Model.One = tbOne.Text;
+            Model.Two = tbTwo.Text;
+            Model.Result = cbOperation.SelectedIndex;
+        }
+    }
+}
---
+++ MVC/Model.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MVC
+{
+    public static class Model
+    {
+        public static List<string> dataListOperationStr = new List<string>() { "Сложение", "Вычитание", "Умножение", "Деление" };
+        public static List<string> dataListOperationZnak = new List<string>() { "+", "-", "*", "/" };
+
+        public static TextBlock tbTextOperation;
+        public static string One;
+        public static string Two;
+        public static TextBox tbResult;
+
+        public static int Operation
+        {
+            set
+            {
+                tbTextOperation.Text = dataListOperationZnak[value];
+            }
+        }
+        public static int Result
+        {
+            set
+            {
+                try
+                {
+                    double numberOne = 0;
+                    double numberTwo = 0;
+                    if (One != "")
+                        numberOne = Convert.ToDouble(One);
+                    if (Two != "")
+                        numberTwo = Convert.ToDouble(Two);
+                    switch (value)
+                    {
+                        case 0:
+                            tbResult.Text = Convert.ToString(numberOne + numberTwo);
+                            break;
+                        case 1:
+                            tbResult.Text = Convert.ToString(numberOne - numberTwo);
+                            break;
+                        case 2:
+                            tbResult.Text = Convert.ToString(numberOne * numberTwo);
+                            break;
+                        case 3:
+                            if (numberTwo == 0)
+                                tbResult.Text = "Деление на 0 невозможно!";
+                            else
+                                tbResult.Text = Convert.ToString(numberOne / numberTwo);
+                            break;
+                    }
+                }
+                catch
+                {
+                    tbResult.Text = "Ошибка!";
+                }
+            }
+        }
+    }
+}
